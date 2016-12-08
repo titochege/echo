a3a4cc87a1f0e924b629794c4c4941e95093e8de
titochege <tito.chege@gmail.com>
2016-12-08 15:02:08 +0000

    Add files via upload

---
+++ part 1.java
@@ -0,0 +1,17 @@
+
+public class part1 {
+	public static void main(String args[])
+	{
+		long x;
+		
+		x = 100;
+		System.out.println(x);
+		x = x + 10; //Line A
+		System.out.println(x);
+		x = x + 20; //Line B
+		System.out.println(x);
+		
+		
+		
+	}
+}
